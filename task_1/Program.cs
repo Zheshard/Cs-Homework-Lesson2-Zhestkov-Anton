982cc581fa5e820c8a43835cdb33b8918b8c580b
Zheshard <antonzestkov489@gmail.com>
2022-12-04 12:32:59 +0300

    Solving task 1 using a conditional operator

--- task_1/Program.cs
+++ task_1/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Программа, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите трехзначное число: ");
+string numberStr = Console.ReadLine();
+
+if (numberStr.Length == 3)
+{
+	int number = Convert.ToInt32(numberStr);
+	int digit2 = (number % 100) / 10;
+	System.Console.WriteLine(digit2);
+}
+else
+{
+	System.Console.WriteLine("Вы ввели неверное число!");
+}
