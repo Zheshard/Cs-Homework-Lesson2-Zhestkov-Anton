cbf9100a0af8bf655aaa4648de49f78e03c20038
Zheshard <antonzestkov489@gmail.com>
2022-12-04 12:55:18 +0300

    Completed task 2

---
+++ task_2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Программа, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.WriteLine("Ввдите число: ");
+string number = Console.ReadLine();
+
+if (number.Length > 2)
+{
+	System.Console.WriteLine(number[2]);
+}
+else
+{
+	System.Console.WriteLine("Третьей цифры нет");
+}
